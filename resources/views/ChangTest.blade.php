fb06aa99924afc5b5c6bba38477d211330b7e467
ChangSiang <alex51688@gmail.com>
2018-10-11 20:40:29 +0800

    update Bootstrap

--- resources/views/ChangTest.blade.php
+++ resources/views/ChangTest.blade.php
@@ -5,14 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{{ asset('css/bootstrap-3.3.7-dist/css/bootstrap.css') }}}">
-    <!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
-    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <!-- 可选的 Bootstrap 主题文件（一般不用引入） -->
-    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <!-- JQuery -3.3.1 一定要放在 Bootstrap.min.js 的前面 -->
     <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
-    <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
-    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="{{asset('css/bootstrap-3.3.7-dist/js/bootstrap.js')}}"></script>
     <title>PestHelper</title>
 </head>
 <body style="margin:20px">
@@ -46,6 +40,5 @@
             </div>
         </div>
     </div>
-
 </body>
 </html>
