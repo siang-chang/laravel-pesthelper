91f80a830c302e2fded0f49dfc899d7b0e63dbc9
Chang Siang <alex51688@gmail.com>
2018-10-13 01:26:20 +0800

    addUnit ".editorconfig"

--- resources/views/ChangTest.blade.php
+++ resources/views/ChangTest.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,23 +10,68 @@
     <script src="{{asset('css/bootstrap-3.3.7-dist/js/bootstrap.js')}}"></script>
     <title>PestHelper</title>
 </head>
-<body style="margin:20px">
-    <!-- <div class="navbar">
-        <div class="navbar-inner">
-            <ul class="nav nav-tabs" id="myTab">
-                <li class="active"><a href="#home" data-toggle="tab">Home</a></li>
-                <li><a href="#news" data-toggle="tab">News</a></li>
-                <li><a href="#blog" data-toggle="tab">blog</a></li>
-                <li><a href="#about" data-toggle="tab">about</a></li>
-            </ul>
-            <div class="tab-content">
-                <div class="tab-pane active" id="home">Home</div>
-                <div class="tab-pane news" id="news">News</div>
-                <div class="tab-pane blog" id="blog">blog</div>
-                <div class=tab-pane id="about">about</div>
-            </div>
-        </div>
-    </div> -->
+
+<body>
+    <header>
+        <!-- 導覽列 -->
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+                        aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Brand</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                        <li><a href="#">Link</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                                aria-expanded="false">Dropdown <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Separated link</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">One more separated link</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="navbar-form navbar-left">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Search">
+                        </div>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Link</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                                aria-expanded="false">Dropdown <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Separated link</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+    </header>
+    <!-- 內容區塊 -->
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
@@ -36,9 +82,38 @@
                 </div>
             </div>
             <div class="col-xs-12 col-sm-9 col-md-7 col-lg-8" style="background-color:blue;color:white">
-                <p><h1>this is a first Bootstrap page</h1></p>
+                <p>
+                    <h1>this is a first Bootstrap page</h1>
+                </p>
             </div>
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
+            <p>4</p>
+            <p>2</p>
+            <p>2</p>
+            <p>1</p>
+            <p>1</p>
+            <p>1</p>
+            <p>1</p>
+            <p>1</p>
+            <p>1</p>
+            <p>1</p>
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
+            <p>4</p>
+            <p>2</p>
+            <p>2</p>
+            <p>1</p>
+            <p>1</p>
+            <p>1</p>
+            <p>1</p>
+            <p>1</p>
+            <p>1</p>
+            <p>1</p>
         </div>
     </div>
 </body>
+
 </html>
