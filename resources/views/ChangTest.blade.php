20dad26b62843e512b602e21e3dfbe35c3055466
Chang Siang <alex51688@gmail.com>
2018-10-16 16:50:42 +0800

    Update "navbar" in "ChangTest.blade.php"

--- resources/views/ChangTest.blade.php
+++ resources/views/ChangTest.blade.php
@@ -11,10 +11,10 @@
     <title>PestHelper</title>
 </head>
 
-<body>
+<body style="padding-top: 70px;">
     <header>
         <!-- 導覽列 -->
-        <nav class="navbar navbar-default navbar-static-top">
+        <nav class="navbar navbar-default navbar-fixed-top">
             <div class="container-fluid">
                 <!-- Brand and toggle get grouped for better mobile display -->
                 <div class="navbar-header">
@@ -25,26 +25,15 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">Brand</a>
+                    <a class="navbar-brand" href="#">蟲害小幫手</a>
                 </div>
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                        <li><a href="#">Link</a></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                                aria-expanded="false">Dropdown <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="#">Separated link</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="#">One more separated link</a></li>
-                            </ul>
+                        <li class="active"><a href="#">害蟲辨識<span class="sr-only">(current)</span></a></li>
+                        <li><a href="#">害蟲目錄</a></li>
+                        <li><a href="#">植株目錄</a></li>
                         </li>
                     </ul>
                     <form class="navbar-form navbar-left">
