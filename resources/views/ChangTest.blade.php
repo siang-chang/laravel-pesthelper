fc4ed2635f3bcb606bf3f46e840a316d83b5695e
ChangSiang <alex51688@gmail.com>
2018-10-18 15:25:08 +0800

    合併前上傳(作業未完)

--- resources/views/ChangTest.blade.php
+++ resources/views/ChangTest.blade.php
@@ -41,6 +41,7 @@
                         <li><a href="#" style="color: #ffffff;">害蟲目錄</a></li>
                         <li><a href="#" style="color: #ffffff;">植株目錄</a></li>
                     </ul>
+                    <!-- search -->
                     {{-- <form class="navbar-form navbar-right">
                         <div class="form-group">
                             <input type="text" class="form-control" placeholder="Search">
@@ -53,39 +54,64 @@
     </header>
     <!-- 內容區塊 -->
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="row">
-                    <h1 class="col-xs-12 col-sm-4" style="background-color:blue;color:white">News</h1>
-                    <h1 class="col-xs-12 col-sm-4" style="background-color:red;color:white">Blog</h1>
-                    <h1 class="col-xs-12 col-sm-4" style="background-color:green;color:white">About</h1>
+        <!-- 網站標語 Web Slogan -->
+        <div class="container-fluid">
+            <h1 id="Web-page-slogan" class="col-xs-12">蟲害小幫手<br />病蟲害解決方案查詢服務</h1>
+        </div>
+        <!-- 功能標語 Function Slogan -->
+        <div class="container-fluid">
+            <h2 id="text-medium">開始你的搜尋，病蟲害問題迎刃而解</h2>
+        </div>
+        <!-- 搜尋列 Search Bar -->
+        <div class="container-fluid">
+            <span class="col-xs-1 col-md-2"></span>
+            <input type="text" id="searchBar" class="col-xs-10 col-md-8">
+            <span class="col-xs-1 com-md-2"></span>
+        </div>
+        <!-- Popular Keyword -->
+        <div class="container-fluid">
+            <div id="popularKeyword" class="col-xs-2 col-md-1">一葉蟲</div>
+            <div id="text-small" class="col-xs-2 col-md-1">二葉蟲</div>
+            <div id="text-small" class="col-xs-2 col-md-1">三葉蟲</div>
+            <div id="text-small" class="col-xs-2 col-md-1">四葉蟲</div>
+            <div id="text-small" class="col-xs-2 col-md-1">五葉蟲</div>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="row">
+                        <h1 class="col-xs-12 col-sm-4" style="background-color:blue;color:white">News</h1>
+                        <h1 class="col-xs-12 col-sm-4" style="background-color:red;color:white">Blog</h1>
+                        <h1 class="col-xs-12 col-sm-4" style="background-color:green;color:white">About</h1>
+                    </div>
                 </div>
             </div>
-        </div>
-        <button type="button" id="btnTest" class="btn-2">PestHelper</button>
-        <script>
-            // $("#btnTest").click(function(){
+            <button type="button" id="btnTest" class="btn-2">PestHelper</button>
+            <script>
+                // $("#btnTest").click(function(){
                 // alert('PestHelper');
             // })
             $("#btnTest").on('click', function(){
                 alert('PestHelperTest');
             })
         </script>
-        <div id="alertTest" class="alert alert-warning alert-dismissible fade in" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <strong>Warning!</strong> Better check yourself, you're not looking too good.
+            <div id="alertTest" class="alert alert-warning alert-dismissible fade in" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <strong>Warning!</strong> Better check yourself, you're not looking too good.
+            </div>
+            <button type="button" id="closeBtn" class="btn btn-default">
+                close
+            </button>
+            <script>
+                $('#closeBtn').on('click', function () {
+                    $('#alertTest').alert('close');
+                })
+                $('#alertTest').on('closed.bs.alert', function () {
+                    alert('消失了');
+                })
+
+            </script>
         </div>
-        <button type="button" id="closeBtn" class="btn btn-default" >
-            close
-        </button>
-        <script>
-            $('#closeBtn').on('click', function(){
-                $('#alertTest').alert('close');
-            })
-            $('#alertTest').on('closed.bs.alert', function(){
-                alert('消失了');
-            })
-        </script>
     </div>
 </body>
 
