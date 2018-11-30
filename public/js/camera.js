18546694b999b5fd851cacced4a535c35faa14b7
ChangSiang <alex51688@gmail.com>
2018-11-30 16:44:40 +0800

    Update camera.js

--- public/js/camera.js
+++ public/js/camera.js
@@ -129,7 +129,7 @@ function dorecognition(imgUrl) {
         success: function (data) {
             console.log(data);
             // location.href= LaravelUrl + '/recognitionfail?userImg=' + data;
-            location.href= LaravelUrl + '/recognitionfail';
+            // location.href= LaravelUrl + '/recognitionfail';
         }
     })
 }
