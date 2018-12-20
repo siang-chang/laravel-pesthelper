79b976b052fcdcd6b95fc2dd89167cfbd54f0542
ChangSiang <alex51688@gmail.com>
2018-12-20 16:29:26 +0800

    Add About.blade.php

---
+++ resources/views/site/about.blade.php
@@ -0,0 +1,29 @@
+@extends('site.master.subpage')
+@section('pageTitle', '關於我們')
+@section('description', '關於我們')
+@section('content')
+
+<!-- 頁面載入動畫 -->
+@include('site.layouts.loadingSpinner')
+
+        <!-- 頁面 Title -->
+        <div class="container">
+            <div class="row page-title">
+                <h1 class="col-xs-12 text-Large-1">關於我們</h1>
+                <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+                    <hr />
+                </div>
+            </div>
+        </div><!-- 頁面 Title 結束 -->
+
+        <!-- 內容區塊 -->
+        <div class="container">
+
+
+
+
+
+        </div><!-- 內容區塊結束 -->
+
+
+    @stop
