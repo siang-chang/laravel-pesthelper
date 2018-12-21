fb698a344b8f8d7a608834db5f04de0ef78e11cd
ChangSiang <alex51688@gmail.com>
2018-12-21 18:42:35 +0800

    Update about.blade.php

--- resources/views/site/layouts/footer.blade.php
+++ resources/views/site/layouts/footer.blade.php
@@ -9,7 +9,7 @@
                 {{-- <a href="https://line.me/R/ti/p/%40viz9496a">
                     <img height="36" border="0" alt="加入好友" src="{{ asset('img/icon/LINE_SOCIAL_Fukidashi_PNG/LINE_SOCIAL_Fukidashi_typeA.png') }}">
                 </a> --}}
-                <a href="{{ url('/about') }}">聯絡我們：npustunknown@gmail.com</a>
+                <a href="{{ url('/about') }}">關於我們</a>
             </div>
 
         </div>
